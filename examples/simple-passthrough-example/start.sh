25e5d9b90f3f0c9667a01d07b67a8071f89bc3ed
Prabha Manepalli <mprabha@confluent.io>
2025-08-18 13:47:34 +0530

    remove local path references

--- examples/simple-passthrough-example/start.sh
+++ examples/simple-passthrough-example/start.sh
@@ -2,9 +2,7 @@
 
 export GATEWAY_IMAGE="519856050701.dkr.ecr.us-west-2.amazonaws.com/docker/prod/confluentinc/gateway:master-latest-ubi9"
 
-export LOCAL_GATEWAY_CONFIG_FILE="/Users/prabhamanepalli/cpc/gateway-images/examples/simple-passthrough-example/gateway.yaml"
-
-export KAFKA_SERVER_JAAS_CONF="/Users/prabhamanepalli/cpc/gateway-images/examples/simple-passthrough-example/kafka_server_jaas.conf"
+export KAFKA_SERVER_JAAS_CONF="$(pwd)/kafka_server_jaas.conf"
 
 docker compose down -v || true
 docker container prune -f || true
