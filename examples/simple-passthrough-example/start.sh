b4b9ecc6cf505dc8db5c26aef4dcbc8a9daa10de
Prabha Manepalli <112160839+prabhamanepalli@users.noreply.github.com>
2025-08-20 19:28:25 +0530

    Consume latest image (#14)
    * updated to consume the latest gateway image from dockerhub

--- examples/simple-passthrough-example/start.sh
+++ examples/simple-passthrough-example/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GATEWAY_IMAGE="519856050701.dkr.ecr.us-west-2.amazonaws.com/docker/prod/confluentinc/gateway:master-latest-ubi9"
+export GATEWAY_IMAGE="confluentinc/cpc-gateway:latest"
 
 export KAFKA_SERVER_JAAS_CONF="$(pwd)/kafka_server_jaas.conf"
 
