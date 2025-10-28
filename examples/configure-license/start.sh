b66bba59a8b2a2c30d4514d54d5d6ad670c2c0ff
Hrishabh Gupta <hgupta@confluent.io>
2025-10-28 17:11:56 +0530

    added example

---
+++ examples/configure-license/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export GATEWAY_IMAGE="confluentinc/cpc-gateway:latest"
+
+export KAFKA_SERVER_JAAS_CONF="$(pwd)/kafka_server_jaas.conf"
+
+docker compose down -v || true
+docker container prune -f || true
+docker compose up -d
